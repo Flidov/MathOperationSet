fa961aae99a8c10ec7199e68b7dbdcb934cacad9
flidov <mrfioden@gmail.com>
2022-07-04 20:42:15 +0300

    Done

--- src/main/java/Calculate.java
+++ src/main/java/Calculate.java
@@ -5,17 +5,20 @@ import java.util.stream.Stream;
 
 public class Calculate {
 
-    public static Set<Given>  union(HashSet<Given> set1, HashSet<Given> set2) {
+    public static Set<Given> union(HashSet<Given> set1, HashSet<Given> set2) {
         return Stream.concat(set1.stream(), set2.stream()).collect(Collectors.toSet());
     }
-    public static Set<Given>  intersection (HashSet<Given> set1, HashSet<Given> set2){
+
+    public static Set<Given> intersection(HashSet<Given> set1, HashSet<Given> set2) {
         return set1.stream().filter(set2::contains).collect(Collectors.toSet());
-     }
-    public static Set<Given>  minus (HashSet<Given> set1, HashSet<Given> set2){
+    }
+
+    public static Set<Given> minus(HashSet<Given> set1, HashSet<Given> set2) {
         return set1.stream().filter(set2::add).collect(Collectors.toSet());
 
     }
-      public static Set<Given>  difference (HashSet<Given> set1, HashSet<Given> set2){
-        return  Stream.concat(minus(set1,set2).stream(),minus(set2,set1).stream()).collect(Collectors.toSet());
-      }
+
+    public static Set<Given> difference(HashSet<Given> set1, HashSet<Given> set2) {
+        return Stream.concat(minus(set1, set2).stream(), minus(set2, set1).stream()).collect(Collectors.toSet());
+    }
 }
--- src/test/java/CalculateTest.java
+++ src/test/java/CalculateTest.java
@@ -15,42 +15,42 @@ public class CalculateTest {
 
 
     @Before
-    public void setUp()  {
-        set1=new HashSet<>(Arrays.asList(Given.A,Given.B));
-        set2=new HashSet<>(Arrays.asList(Given.B,Given.C));
+    public void setUp() {
+        set1 = new HashSet<>(Arrays.asList(Given.A, Given.B));
+        set2 = new HashSet<>(Arrays.asList(Given.B, Given.C));
     }
 
     @After
-    public void tearDown()  {
-        set1=null;
-        set2=null;
+    public void tearDown() {
+        set1 = null;
+        set2 = null;
     }
 
     @Test
     public void union() {
-        HashSet<Given> expect=new HashSet<>(Arrays.asList(Given.A,Given.B,Given.C));
-        Set<Given> actual=Calculate.union(set1,set2);
-        assertEquals(actual,expect);
+        HashSet<Given> expect = new HashSet<>(Arrays.asList(Given.A, Given.B, Given.C));
+        Set<Given> actual = Calculate.union(set1, set2);
+        assertEquals(actual, expect);
     }
 
     @Test
     public void intersection() {
-        HashSet<Given> expect=new HashSet<>(List.of(Given.B));
-        Set<Given> actual= Calculate.intersection(set1,set2);
-        assertEquals(actual,expect);
+        HashSet<Given> expect = new HashSet<>(List.of(Given.B));
+        Set<Given> actual = Calculate.intersection(set1, set2);
+        assertEquals(actual, expect);
     }
 
     @Test
     public void minus() {
-        HashSet<Given> expect=new HashSet<>(List.of(Given.A));
-        Set<Given> actual= Calculate.minus(set1,set2);
-        assertEquals(actual,expect);
+        HashSet<Given> expect = new HashSet<>(List.of(Given.A));
+        Set<Given> actual = Calculate.minus(set1, set2);
+        assertEquals(actual, expect);
     }
 
     @Test
     public void difference() {
-        HashSet<Given> expect=new HashSet<>(Arrays.asList(Given.A,Given.C));
-        Set<Given> actual= Calculate.difference(set1,set2);
-        assertEquals(actual,expect);
+        HashSet<Given> expect = new HashSet<>(Arrays.asList(Given.A, Given.C));
+        Set<Given> actual = Calculate.difference(set1, set2);
+        assertEquals(actual, expect);
     }
 }
